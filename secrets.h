4f2948285c031f3094d31f0751c7135967da87af
Melisa0908 <barbaramelisa0908@gmail.com>
2024-11-02 00:00:13 -0500

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "AAA"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "aaa"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "AAA123"   // replace XYZ with your channel write API Key
